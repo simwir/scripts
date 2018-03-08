e158a86888e16c2dee4a5c41d94d2f8c5b377dea
Nikolaj Jensen Ulrik <nikolaj.ulrik@gmail.com>
2018-03-08 15:02:57 +0100

    Escape backslashes

--- GithubToSlack.php
+++ GithubToSlack.php
@@ -49,6 +49,7 @@ if($string !== NULL){
 			foreach($commit_info['files'] as $file){
 				$lines = explode("\n", $file['patch']);
 				foreach($lines as $line){
+					$line = str_replace("\\", "\\\\", $line);
 					$line = remove_todos($line);
 					if(str_split($line)[0] === '+'){
 						if(preg_match('/[\s+][mM]an[\W]/', $line)  === 1){
