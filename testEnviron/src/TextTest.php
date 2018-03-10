b10a696c57c449f0da9f51a5b69fbc0ab124d1cd
Nikolaj Jensen Ulrik <nikolaj.ulrik@gmail.com>
2018-03-10 12:48:07 +0100

    Fix unicode related bugs. Included is a barebones test-file

---
+++ testEnviron/src/TextTest.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: waefwerf
+ * Date: 3/8/18
+ * Time: 12:19 PM
+ */
+
+use PHPUnit\Framework\TestCase;
+
+include "texthandling.php";
+
+class TextTest extends TestCase
+{
+    function testNoTodos()
+    {
+        $inputstr = "Hello world";
+        $this->assertTrue($inputstr === remove_todos($inputstr));
+    }
+
+    function testRemoveSingleTodo()
+    {
+        $inputstr = "Hello \\todo{goodbye}";
+        $res = remove_todos($inputstr);
+        $this->assertTrue(remove_todos($inputstr) === "Hello ");
+    }
+
+    function testHandlesNestedBraces()
+    {
+        $inputstr = "This is nested\\todo{{hello}}";
+        $res = remove_todos($inputstr);
+        $this->assertTrue($res == "This is nested");
+    }
+
+    function testHandlesMultipleTodos()
+    {
+        $inputstr = "Hi\\todo{hello} there\\todo{lel}";
+        $res = remove_todos($inputstr);
+        $this->assertTrue($res === "Hi there");
+    }
+
+    function testHandlesOptArgs()
+    {
+        $inputstr = "Optional \\todo[asd]{adadsdaw}";
+        $res = remove_todos($inputstr);
+        $this->assertTrue($res === "Optional ");
+    }
+
+    function testSpaceBeforeBrace()
+    {
+        $inputstr = "Optional \\todo[asd] {adadsdaw}";
+        $res = remove_todos($inputstr);
+        $this->assertTrue($res === "Optional ");
+    }
+
+    function testSpaceBeforeOptArgs()
+    {
+        $inputstr = "Optional \\todo [asd]{adadsdaw}";
+        $res = remove_todos($inputstr);
+        $this->assertTrue($res === "Optional ");
+    }
+
+    function testTextAfterTodo()
+    {
+        $inputstr = "\\todo{asdlkajd} der skal være tekst her.";
+        $res = remove_todos($inputstr);
+        $this->assertTrue($res === " der skal være tekst her.");
+    }
+
+    function testEmptyString()
+    {
+        $inputstr = "";
+        $res = remove_todos($inputstr);
+        $this->assertTrue($res === "");
+        $inputstr = '';
+        $res = remove_todos($inputstr);
+        $this->assertTrue($res === '');
+    }
+    function testEmptyStringPlus()
+    {
+        $inputstr = "+";
+        $res = remove_todos($inputstr);
+        $this->assertTrue($res === "+");
+    }
+    function testManhunt_NoMans()
+    {
+        $inputstr = "Der er intet galt her";
+        echo manhunt($inputstr);
+        $this->assertTrue(manhunt($inputstr) === false);
+    }
+
+    function testManhunt_CapitalMan()
+    {
+        $inputstr = "+ Man burde teste";
+        $this->assertTrue(manhunt($inputstr) !== false);
+    }
+
+    function testManhunt_lowercaseMan()
+    {
+        $inputstr = "+ Hvis man ser her";
+        $this->assertTrue(manhunt($inputstr) !== false);
+    }
+
+    function testManhunt_doesntRemoveTodos()
+    {
+        $inputstr = "+ Man burde teste det her\todo{asd}";
+        echo($inputstr);
+        $this->assertTrue(manhunt($inputstr) === "+ Man burde teste det her\\todo{asd}");
+    }
+    function testManhuntRealInput()
+    {
+        $inputstr = "\renewcommand{\\thepage}{\alphalph{\value{page}}}";
+        $this->assertTrue(manhunt($inputstr) === false);
+        $inputstr = "		\includegraphics[width=\linewidth]{figures/dotexample.pdf}\n";
+        $this->assertTrue(manhunt($inputstr) === false);
+    }
+    function testRemovesTodosInComments()
+    {
+        $inputstr = "%\\todo[inline]{Syntes der bliver brugt for lidt energi på biblioteket. Hvorfor er der ikke et kodeeksempel på dette, og hvilke fordele/ulemper er der ved det? Hvis Java eller CS fikser de nævnte problemer, hvorfor er de så ikke overvejet nærmere?}";
+        $this->assertTrue(remove_todos($inputstr) == "%");
+    }
+    function testDumbLine()
+    {
+        $inputstr = '+%Sidst er sproget meget specifikt, så det kræver en grafisk brugergrænseflade, og det er svært at generalisere algoritmer\todo{Citation needed}, hvoraf sidstnævnte vil være en fordel ved f.eks. Dijkstras algoritme, jf. afsnit \ref{sec:problemanalyse_graphproblems_shortestpath}\todo{Ref til Dijkstra afsnit når merged}, hvor vægten på en kant kan opfattes som en variabel på hver kant eller en funktion for vægten mellem to knuder.';
+        $expect = '+%Sidst er sproget meget specifikt, så det kræver en grafisk brugergrænseflade, og det er svært at generalisere algoritmer, hvoraf sidstnævnte vil være en fordel ved f.eks. Dijkstras algoritme, jf. afsnit \ref{sec:problemanalyse_graphproblems_shortestpath}, hvor vægten på en kant kan opfattes som en variabel på hver kant eller en funktion for vægten mellem to knuder.';
+        $this->assertTrue(remove_todos($inputstr) === $expect);
+    }
+    function testManhuntGitDiff()
+    {
+        $inputstr = file_get_contents("gitdiff", FILE_USE_INCLUDE_PATH);
+        $lines = explode("\n", $inputstr);
+        for ($i = 1136; $i < count($lines); ++$i) {
+            echo(sprintf("Lines: %s\tLength: %d\n", $i+1, strlen($lines[$i])));
+            manhunt($lines[$i]);
+        }
+        $this->assertTrue(true);
+    }
+}
+
+
---
+++ testEnviron/src/texthandling.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: waefwerf
+ * Date: 3/8/18
+ * Time: 11:21 AM
+ */
+
+function char_at($str, $pos) {
+    return mb_substr($str, $pos, 1);
+}
+
+function remove_todos($line)
+{
+    return remove_todos_old($line);
+}
+
+
+function remove_todos_old($line) {
+    $len = mb_strlen($line);
+    $pos = mb_strpos($line, "\\todo");
+    $stack = new SplStack();
+    $startparens = "([{";
+    $endparens = ")]}";
+    while ($pos < $len && $pos !== false  ) {
+        echo($line."\n");
+        echo(sprintf("pos == %s\n", $pos));
+        $startpos = $pos;
+        echo("Character at pos: ".char_at($line,$pos)."\n");
+        echo("Startpos = ".$startpos."\n");
+        /* Skip to first parenthesis */
+        while (mb_strpos($startparens, char_at($line, $pos)) === false) {
+            $pos++;
+            echo($pos." ");
+        }
+        /* Skip optional parameters */
+        if (char_at($line, $pos) == "[") {
+            $pos = mb_strpos($line, "]", $pos) + 1;
+//            while (char_at($line, $pos) !== "]") {
+//                $pos++;
+//            }
+//            echo(sprintf("charat pos %d == %s", $pos, char_at($line, $pos)));
+//            $pos++;
+            //$stack->pop();
+        }
+        //Skip to opening brace
+        while (char_at($line, $pos) !== "{") {
+            $pos++;
+        }
+        //$pos++;
+        $stack->push(char_at($line, $pos));
+        while (!$stack->isEmpty()) {
+            $pos++;
+            if (char_at($line, $pos) === "{") {
+                $stack->push(char_at($line, $pos));
+            }
+            elseif (char_at($line, $pos) === "}") {
+                $stack->pop();
+            }
+        }
+        echo(sprintf("Substring \"%s\" at pos %d to %d\n", $line, $startpos, $pos));
+        $line = str_replace(mb_substr($line, $startpos, $pos - $startpos + 1), "", $line);
+        echo(sprintf("After removal: %s\n", $line));
+        $pos = mb_strpos($line, "\\todo");
+    }
+    return $line;
+}
+function remove_todos_new($line)
+{
+    $len = strlen($line);
+    $pos = strpos($line, '\todo');
+    $stack = new SplStack();
+    while ($pos < $len && $pos !== false) {
+        echo("Todo found in line ".$line."\n");
+        $todoPos = $pos;
+        $bracePos = strpos($line, '{', $pos);
+        $pos = $bracePos;
+        $stack->push(char_at($line, $pos));
+        while (!$stack->isEmpty()) {
+            $pos++;
+            if (char_at($line, $pos) === '{') {
+                $stack->push('{');
+            }
+            if (char_at($line, $pos) === '}') {
+                $stack->pop();
+            }
+        }
+        $line = str_replace(mb_substr($line, $todoPos, $pos - $todoPos + 1), "", $line);
+        $pos = strpos($line, '\todo');
+        echo("Post removal: ".$line."\n");
+    }
+    return $line;
+}
+
+
+function manhunt($line) {
+    $line = str_replace("\t", "\\t",$line);
+    echo(sprintf("Filtered line: %s\n", $line));
+    $filtered_line = remove_todos($line);
+    if(str_split($filtered_line)[0] === '+'){
+        if(preg_match('/[\s+][mM]an[\W]/', $filtered_line)  === 1){
+            echo(sprintf("%s\n", $line));
+            return $line;
+        }
+        /*if(preg_match('/[\s+][Vv]i\W/', $line)  === 1){
+            $vi_lines[] = $line;
+            echo $line;
+        }*/
+    }
+    echo("No mans found\n");
+    return false;
+}
+
